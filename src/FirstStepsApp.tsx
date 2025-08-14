efbfd4a7f037e27109d62ea873ed94afdda3a481
Diego Caucota <diegocaucota@gmail.com>
2025-08-14 12:48:11 -0300

    feat: ItemCounter props

--- src/FirstStepsApp.tsx
+++ src/FirstStepsApp.tsx
@@ -1,14 +1,14 @@
+import { ItemCounter } from './shopping-cart/ItemCounter';
+
 export function FirstStepsApp() {
   return (
     <>
-      <h1>Hola Mundo!!</h1>
-      <p>Esto es un parrafo!!</p>
-      <button>Click me</button>
-      <div>
-        <div>
-          <h2>Hola dentro de un div</h2>
-        </div>
-      </div>
+      <h1>Carrito de Compras</h1>
+
+      <ItemCounter productName='Nintendo Switch 2' quantity={5} />
+      <ItemCounter productName='Play 5' quantity={3} />
+      <ItemCounter productName='Attari 2000' quantity={6} />
+      <ItemCounter productName='Super Mario 2' quantity={4} />
     </>
   );
 }
---
+++ src/shopping-cart/ItemCounter.tsx
@@ -0,0 +1,22 @@
+interface ItemCounterProps {
+  productName: string;
+  quantity: number;
+}
+
+export const ItemCounter = ({ productName, quantity }: ItemCounterProps) => {
+  return (
+    <section
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        gap: 10,
+        marginTop: 10,
+      }}
+    >
+      <span style={{ width: 150 }}>{productName}</span>
+      <button>+1</button>
+      <span>{quantity}</span>
+      <button>-1</button>
+    </section>
+  );
+};
