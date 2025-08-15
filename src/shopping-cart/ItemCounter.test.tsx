644e6df3cdc22fec29d13662eea1b02c811cf589
Diego Caucota <diegocaucota@gmail.com>
2025-08-15 09:13:44 -0300

    add: test functions add and subtract

--- src/shopping-cart/ItemCounter.test.tsx
+++ src/shopping-cart/ItemCounter.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { describe, expect, test } from 'vitest';
 import { ItemCounter } from './ItemCounter';
 
@@ -23,5 +23,42 @@ describe('ItemCounter', () => {
 
     //! ASSERT
     expect(screen.getByText(quantity)).toBeDefined();
+  });
+
+  test('should increase count when +1 button is pressed', () => {
+    render(<ItemCounter itemName={'Test item'} quantity={1} />);
+
+    const [buttonAdd] = screen.getAllByRole('button');
+    // Para disparar un evento usamos "fireEvent" de Testing Library
+    //! ACT
+    fireEvent.click(buttonAdd);
+
+    //! ASSERT
+    expect(screen.getByText('2')).toBeDefined();
+  });
+
+  test('should decrease count when -1 button is pressed', () => {
+    const quantity = 5;
+    render(<ItemCounter itemName={'Test item'} quantity={quantity} />);
+
+    const [, buttonSubtract] = screen.getAllByRole('button');
+    // Para disparar un evento usamos "fireEvent" de Testing Library
+    //! ACT
+    fireEvent.click(buttonSubtract);
+
+    //! ASSERT
+    expect(screen.getByText('4')).toBeDefined();
+  });
+
+  test('should not decrease count when -1 button is pressed and quantity is 1', () => {
+    const quantity = 1;
+    render(<ItemCounter itemName={'Test item'} quantity={quantity} />);
+    const [, buttonSubtract] = screen.getAllByRole('button');
+    // Para disparar un evento usamos "fireEvent" de Testing Library
+    //! ACT
+    fireEvent.click(buttonSubtract);
+
+    //! ASSERT
+    expect(screen.getByText('1')).toBeDefined();
   });
 });
