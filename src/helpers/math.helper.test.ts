1c729e70413cda48397001d2e548815745f485b9
Diego Caucota <diegocaucota@gmail.com>
2025-08-15 15:49:58 -0300

    add: describe test

--- src/helpers/math.helper.test.ts
+++ src/helpers/math.helper.test.ts
@@ -1,16 +1,71 @@
 // Importar test desde vitest
-import { expect, test } from "vitest";
-import { add } from "./math.helper";
+import { describe, expect, test } from 'vitest';
+import { add, multiply, subtract } from './math.helper';
 
-test('Should add two positives numbers', () => {
-    // ! 1. Arrange
-    const a = 1
-    const b = 2
+describe('add', () => {
+    test('Should add two positives numbers', () => {
+        // ! 1. Arrange
+        const a = 1;
+        const b = 2;
 
-    // ! 2. Act
-    const result = add(a, b)
+        // ! 2. Act
+        const result = add(a, b);
 
-    // ! 3. Assert
-    // Se lee: Se espera que el "resultado" sea...
-    expect(result).toBe(a + b);
-}); 
+        // ! 3. Assert
+        // Se lee: Se espera que el "resultado" sea...
+        expect(result).toBe(a + b);
+    });
+
+    test('Should add two negatives numbers', () => {
+        // ! 1. Arrange
+        const a = -2;
+        const b = -4;
+
+        // ! 2. Act
+        const result = add(a, b);
+
+        // ! 3. Assert
+        // Se lee: Se espera que el "resultado" sea...
+        expect(result).toBe(a + b);
+    });
+});
+
+describe('subtractor', () => {
+    test('Should subtract two positives numbers', () => {
+        const a = 2;
+        const b = 2;
+
+        const result = subtract(a, b);
+
+        expect(result).toBe(a - b);
+    });
+
+    test('Should subtract one negative and one positive numbers', () => {
+        const a = 2;
+        const b = -2;
+
+        const result = subtract(a, b);
+
+        expect(result).toBe(a - b);
+    });
+});
+
+describe('multiply', () => {
+    test('Should multiply two positives numbers', () => {
+        const a = 2;
+        const b = 4;
+
+        const result = multiply(a, b);
+
+        expect(result).toBe(a * b);
+    });
+
+    test('Should multiply 0 x one positives numbers', () => {
+        const a = 2;
+        const b = 0;
+
+        const result = multiply(a, b);
+
+        expect(result).toBe(a * b);
+    });
+});
