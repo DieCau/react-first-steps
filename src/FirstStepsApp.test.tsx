66ed601edfbdb875f97e4c53d5de95a5ee3f0752
Diego Caucota <diegocaucota@gmail.com>
2025-08-16 09:52:11 -0300

    add: FirstStepsApp mock children component

--- src/FirstStepsApp.test.tsx
+++ src/FirstStepsApp.test.tsx
@@ -1,10 +1,24 @@
-import { describe, expect, test } from 'vitest';
+import { describe, expect, test, vi } from 'vitest';
 import { FirstStepsApp } from './FirstStepsApp';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+
+// Creamos un mock ficticio del componente hijo
+vi.mock('./shopping-cart/ItemCounter', () => ({
+  ItemCounter: () => <div data-testid='ItemCounter' />,
+}));
 
 describe('FirstStepApp', () => {
   test('should match snapshot', () => {
     const { container } = render(<FirstStepsApp />);
     expect(container).toMatchSnapshot();
+  });
+
+  test('should render the correct number of ItemCounter components', () => {
+    render(<FirstStepsApp />);
+
+    const itemCounters = screen.getAllByTestId('ItemCounter');
+
+    //! ASSERT
+    expect(itemCounters.length).toBe(3);
   });
 });
