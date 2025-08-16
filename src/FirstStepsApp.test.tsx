9bbf2e1a6b01e34610a81f10ee08f3e6497a3f5c
Diego Caucota <diegocaucota@gmail.com>
2025-08-16 12:17:22 -0300

    add: MyAwesomeApp testing mocks

--- src/FirstStepsApp.test.tsx
+++ src/FirstStepsApp.test.tsx
@@ -1,15 +1,37 @@
-import { describe, expect, test, vi } from 'vitest';
-import { FirstStepsApp } from './FirstStepsApp';
 import { render, screen } from '@testing-library/react';
+import { afterEach, describe, expect, test, vi } from 'vitest';
+import { FirstStepsApp } from './FirstStepsApp';
 
-// Creamos un mock ficticio del componente hijo
+// Creamos un componente externo donde se extrae el componente ItemCounter
+// vi.fn es una funcion que simula return
+const mockItemCounter = vi.fn((_props: unknown) => {
+  return <div data-testid='ItemCounter' />;
+});
+
 vi.mock('./shopping-cart/ItemCounter', () => ({
-  ItemCounter: () => <div data-testid='ItemCounter' />,
+  ItemCounter: (props: unknown) => mockItemCounter(props),
 }));
 
+// Creamos un mock ficticio del componente hijo
+// vi.mock('./shopping-cart/ItemCounter', () => ({
+// ItemCounter: (props: unknown) => (
+// <div
+//   data-testid='ItemCounter'
+//   name={props.itemName}
+//   quantity={props.quantity}
+// />
+//  ),
+// }));
+
 describe('FirstStepApp', () => {
+  //! Despues de cada prueba limpiar todos los mocks con "vi.clearAllMocks"
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
   test('should match snapshot', () => {
     const { container } = render(<FirstStepsApp />);
+
     expect(container).toMatchSnapshot();
   });
 
@@ -20,5 +42,24 @@ describe('FirstStepApp', () => {
 
     //! ASSERT
     expect(itemCounters.length).toBe(3);
+    // screen.debug();
+  });
+
+  test('should render ItemCounter with correct props', () => {
+    render(<FirstStepsApp />);
+
+    expect(mockItemCounter).toHaveBeenCalledTimes(3);
+    expect(mockItemCounter).toHaveBeenCalledWith({
+      itemName: 'Nintendo Switch 2',
+      quantity: 2,
+    });
+    expect(mockItemCounter).toHaveBeenCalledWith({
+      itemName: 'Mario Bros 3',
+      quantity: 5,
+    });
+    expect(mockItemCounter).toHaveBeenCalledWith({
+      itemName: 'Play 5',
+      quantity: 3,
+    });
   });
 });
